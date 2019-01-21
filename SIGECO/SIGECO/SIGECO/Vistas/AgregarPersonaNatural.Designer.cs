e2ee2727e9acb99e6fa0f484d94e58d34f42ab0a
Jose <burmamachoy@tutanota.com>
2019-01-21 13:50:07 -0800

    Update AgregarPersonaNatural.Designer.cs
    Cambio de cédula

--- SIGECO/SIGECO/SIGECO/Vistas/AgregarPersonaNatural.Designer.cs
+++ SIGECO/SIGECO/SIGECO/Vistas/AgregarPersonaNatural.Designer.cs
@@ -216,7 +216,7 @@
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 16);
             this.label6.TabIndex = 12;
-            this.label6.Text = "*Cedula";
+            this.label6.Text = "*Cédula";
             // 
             // label5
             // 
